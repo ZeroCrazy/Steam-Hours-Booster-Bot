8afd88a65f5421f5e8a1ca64f4438f174f57ba49
Daniel Garzón <daniel.garzon.delgado@gmail.com>
2024-07-29 00:43:15 +0200

    Create start.bat

---
+++ start.bat
@@ -0,0 +1,8 @@
+@echo off
+REM --------------------------------------------------
+REM Steam Hours Booster Bot - Start Script
+REM --------------------------------------------------
+
+node steam_app.js
+
+pause
